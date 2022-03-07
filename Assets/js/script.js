8978dd432766a5ef8113f849124db8d9ef364643
Andreas Kaiafas <akaiafas526@gmail.com>
2022-03-07 17:38:26 -0500

    updated css and finished javascript

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,9 +1,9 @@
 let textArea = document.querySelectorAll('textarea');
-let saveButtons = document.querySelectorAll('.saveBtn');
+let saveButtons = document.querySelectorAll('.save-btn');
 let currentDay = document.getElementById('currentDay');
 let allRows = document.querySelectorAll('.input-group');
 
-let allIds = ['10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM']
+let allIds = ['10:00 AM', '11:00 AM', '12:00 PM', '13:00 PM', '14:00 PM', '15:00 PM', '16:00 PM', '17:00 PM']
 
 for (let i = 0; i < allIds.length; i++) {
     let localStorageData = JSON.parse(localStorage.getItem(allIds[i]));
@@ -12,6 +12,7 @@ for (let i = 0; i < allIds.length; i++) {
 }
 
 for (let i = 0; i < saveButtons.length; i++) {
+    console.log(saveButtons[i])
     saveButtons[i].addEventListener('click', function (event) {
         let target;
         if (event.target.nodeName === "BUTTON") {
@@ -29,8 +30,28 @@ for (let i = 0; i < saveButtons.length; i++) {
 
     })
 }
+
+
+
 let updatedDate = moment().format('YYYY-MM-DD HH:mm:ss');
-console.log(currentDate);
+// console.log(updatedDate);
+
+let currentTime = moment().format('H');
+console.log(currentTime)
+
+for (let i = 0; i < allRows.length; i++ ){
+    let chosenDiv = allRows[i];
+    console.log(chosenDiv.children)
+    let divID = chosenDiv.id.split(':')[0];
+    // console.log(divID)
+    if (parseInt(currentTime) === parseInt(divID)) {
+     chosenDiv.children[1].setAttribute('class','form-control present')
+    }
+    else if (parseInt(currentTime) > parseInt(divID)) {
+        chosenDiv.children[1].setAttribute('class','form-control past')
+    }
+    else {chosenDiv.children[1].setAttribute('class','form-control future')}
+}
 
 setInterval(() => {
     updatedDate = moment().format('YYYY-MM-DD HH:mm:ss');
